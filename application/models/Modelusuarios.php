65a803585a1a9a4f95f35f0053d6cda55c626c12
anghelosieben <anghelosieben13@gmail.com>
2019-06-23 15:50:51 -0500

    Update Modelusuarios.php
    modificando para que funcione

--- application/models/Modelusuarios.php
+++ application/models/Modelusuarios.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Modelusuarios extends CI_Model {
 
 	public function __construct()
-	{	parent::__construct();
+	{	//parent::__construct();
 		$this->load->database();		
 	}
 	public function login($user,$password){
