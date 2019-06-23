4c440312277df4441222ec6adf6ec23c8d532816
anghelosieben <anghelosieben13@gmail.com>
2019-06-23 15:58:15 -0500

    quitando el __construct
    quitando el __construct, por problemas

--- application/models/Modelcategoria.php
+++ application/models/Modelcategoria.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Modelcategoria extends CI_Model {
 
 	public function __construct()
-	{	parent::__construct();
+	{	//parent::__construct();
 		$this->load->database();		
 	}
     public function listar()
