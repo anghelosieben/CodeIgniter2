5f8788bfe0908c72defb48f4d1b8b837f74fff9c
anghelosieben <anghelosieben13@gmail.com>
2019-07-11 20:56:09 -0400

    revisar las consultas
    se hace un revision de las consultas

--- application/models/Modelventas.php
+++ application/models/Modelventas.php
@@ -19,5 +19,25 @@ class Modelventas extends CI_Model {
 	{
 		$this->db->insert("ventas",$data);
 	}
+	public function listar()
+	{	$query=$this->db->query("SELECT v.id_venta,v.fecha,v.total,v.numero_doc,d.nombres,d.apellidos,d.ci FROM ventas v, dpersonales d WHERE v.id_cliente=d.id_dpersonales
+		ORDER BY fecha desc");
+		return $query;
+	}
+	public function listar2()
+	{	$query=$this->db->query("SELECT v.id_venta,v.fecha,v.total,v.id_cliente,v.id_usuario,v.numero_doc,d.nombres,d.apellidos,d.ci FROM ventas v,dpersonales d
+		WHERE v.id_cliente=d.id_dpersonales ORDER BY v.fecha DESC");
+		return $query;
+	}
+	public function mostrarventa($id)
+	{ 	$query=$this->db->query("SELECT * FROM ventas v,dpersonales dp WHERE v.id_venta=$id and v.id_cliente=dp.id_dpersonales");
+		//$query=$this->db->query("SELECT * FROM detalle_ventas d,ventas v,productos p,dpersonales dp WHERE d.venta_id=v.id_venta and d.producto_id=p.id_producto and v.id_cliente=dp.id_dpersonales");
+		return $query;
+	}
+	public function mostrardetalle($id)
+	{ $query=$this->db->query("SELECT d.venta_id,d.precio,d.cantidad,d.importe,p.descripcion FROM detalle_ventas d,productos p WHERE d.producto_id=p.id_producto and d.venta_id=$id");
+		//"SELECT * FROM detalle_ventas d,ventas v,productos p,dpersonales dp WHERE v.id_venta=$id and d.venta_id=v.id_venta and d.producto_id=p.id_producto and v.id_cliente=dp.id_dpersonales");
+		return $query;
+	}
 }
 ?>
